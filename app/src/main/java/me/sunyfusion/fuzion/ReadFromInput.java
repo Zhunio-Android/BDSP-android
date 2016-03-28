81542e66e5441fb478d653f30a48bd36c504996d
Rob <wielandr1@gmail.com>
2016-03-28 18:32:35 -0400

    This class will read from the buildApp.txt file and will return information from that text file.

---
+++ app/src/main/java/me/sunyfusion/fuzion/ReadFromInput.java
@@ -0,0 +1,38 @@
+package me.sunyfusion.fuzion;
+
+import java.io.File;
+
+/**
+ * Created by Robert Wieland on 3/28/16.
+ * This class will read from the buildApp.txt and will return info
+ * from that text file.
+ */
+
+public class ReadFromInput
+{
+    private String Type;    // To hold the type of catagory from the buildApp.txt when read.
+    private String Name;    // To hold the unique catagory name from the buildApp.txt when read.
+    private File file;
+
+
+    public ReadFromInput()
+    {
+
+    }
+
+    public String getCatagoryFromFile()
+    {
+
+    }
+
+    public int getAnswer()
+    {
+
+    }
+
+    public String getUnigueName()
+    {
+
+    }
+
+}
