54286f3f87a946269c52952d3e46e1398f2048a0
Rob <wielandr1@gmail.com>
2016-03-28 19:37:27 -0400

    Worked more on ReadFromInput class.

--- app/src/main/java/me/sunyfusion/fuzion/ReadFromInput.java
+++ app/src/main/java/me/sunyfusion/fuzion/ReadFromInput.java
@@ -1,6 +1,9 @@
 package me.sunyfusion.fuzion;
 
+import java.io.*;
 import java.io.File;
+import java.util.Scanner;
+
 
 /**
  * Created by Robert Wieland on 3/28/16.
@@ -10,29 +13,62 @@ import java.io.File;
 
 public class ReadFromInput
 {
-    private String Type;    // To hold the type of catagory from the buildApp.txt when read.
-    private String Name;    // To hold the unique catagory name from the buildApp.txt when read.
-    private File file;
+    File input = new File("buildApp.txt");
+    Scanner infile = new Scanner(input);
 
+    private String Type = null;    // To hold the type of category from the buildApp.txt when read.
+    private String Name = null;    // To hold the unique category name from the buildApp.txt when read.
+    private int answer = 0;        // To hold the answer to categories such as camera. 1 = yes, 0 = no
 
-    public ReadFromInput()
+    public void ReadLineCollectInfo() throws FileNotFoundException
     {
+        String Line;
+
+        if (infile.hasNextLine())
+        {
+            Line = infile.nextLine();   // gets the line
+            Scanner line = new Scanner(Line); // sets Scanner to that line
+            if(line.hasNext())
+            {
+                Type = line.next(); // Gets the category and assigns to Type
+                if(line.hasNextInt())
+                {
+                    answer = line.nextInt();
+                }
+
+                else if(line.hasNext())
+                    Name = line.next(); // Gets unique name of category
+
+                else    // Final category (endFile)
+                {
+                    Name = null;
+                    answer = 0;
+                }
+
+            }
+
+        }
+
+        // Resets variables to read the next line fresh.
+        Type = null;
+        Name = null;
+        answer = 0;
 
     }
 
-    public String getCatagoryFromFile()
+    public String getCategoryFromFile()
     {
-
+        return Type;
     }
 
     public int getAnswer()
     {
-
+        return answer;
     }
 
     public String getUnigueName()
     {
-
+        return Name;
     }
 
 }
