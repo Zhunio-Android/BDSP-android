bf5a5cbd38b14b846563184ed6cd5943c2981856
Rob <wielandr1@gmail.com>
2016-03-28 18:34:38 -0400

    This class will build the SQLite Database to store the input data from the user, gps, and camera.

---
+++ app/src/main/java/me/sunyfusion/fuzion/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package me.sunyfusion.fuzion;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Robert Wieland on 3/26/16.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+
+    public static final String DATABASE_NAME = "Tasks.db";
+    public static final String TABLE_NAME = "tasksTable";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY)")
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+
+    }
+
+
+    // Method will create new columns in the table of the database
+    public void addColumn(SQLiteDatabase db, String columnName, String columnType)
+    {
+        db.execSQL("ALTER TABLE " + TABLE_NAME + " ADD COLUMN " + columnName + " " + columnType);
+    }
+}
