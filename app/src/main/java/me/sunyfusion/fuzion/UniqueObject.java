1a4840339ff77b9cd7730ab42382c58bde81af5f
Rob <wielandr1@gmail.com>
2016-04-08 13:38:03 -0400

    made unique object to store fields for submission to sqlite data base.

---
+++ app/src/main/java/me/sunyfusion/fuzion/UniqueObject.java
@@ -0,0 +1,29 @@
+package me.sunyfusion.fuzion;
+
+/**
+ * Created by Robert Wieland on 4/8/16.
+ */
+public class UniqueObject
+{
+    private String Name = null;
+    private String Catagory = null;
+
+    public UniqueObject(String cat, String name)
+    {
+        Catagory = cat;
+        Name = name;
+    }
+
+    public String getName()
+    {
+        return Name;
+    }
+
+    public String getCatagory()
+    {
+        return Catagory;
+    }
+
+
+
+}
