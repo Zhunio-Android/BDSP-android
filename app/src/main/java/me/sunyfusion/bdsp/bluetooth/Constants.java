8b968176aa94a7d3075060d9fa83959542f2a410
Bryan Martinez <martineb1@hawkmail.newpaltz.edu>
2016-11-16 20:18:12 -0500

    Bluetooth functionality added (AsyncTask)

--- app/src/main/java/me/sunyfusion/bdsp/bluetooth/Constants.java
+++ app/src/main/java/me/sunyfusion/bdsp/bluetooth/Constants.java
@@ -12,18 +12,19 @@ public class Constants {
     public static final String BUTTON_TEXT = "Get Data";
 
     // Handler message options
-    public static final int NO_BT_ADAPTER = 0;
-    public static final int WAITING_FOR_CLIENT = 1;
-    public static final int MESSAGE_READ = 2;
-    public static final int MESSAGE_READ_COMPLETE = 3;
-    public static final int ERROR = 4;
+    public static final int WAITING_FOR_CLIENT = 0;
+    public static final int MESSAGE_READ_COMPLETE = 1;
+    public static final int SUCCESS = 2;
+    public static final int ERROR = 3;
+    public static final int ERROR_TIMEOUT = 4;
 
     // User communication messages
     public static final String ENABLED = "Bluetooth enabled";
-    public static final String READ_COMPLETE = "Successfully read data";
-    public static final String ALERT_TITLE = "Alert";
-    public static final String WAITING_MESSAGE = "Waiting for client connection";
+    public static final String WAITING_MESSAGE = "Waiting for client connection (30s)";
+    public static final String READ_COMPLETE_MESSAGE = "Successfully read data";
+
     public static final String ERROR_TITLE = "Error";
-    public static final String ERROR_MESSAGE_01 = "Device does not support Bluetooth";
-    public static final String ERROR_MESSAGE_02 = "Unable to establish Bluetooth connection";
+    public static final String ERROR_MESSAGE_00 = "Device does not support Bluetooth";
+    public static final String ERROR_MESSAGE_01 = "Unable to establish Bluetooth connection";
+    public static final String ERROR_MESSAGE_02 = "Connection timed out, try again";
 }
